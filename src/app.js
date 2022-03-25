624580a8e4da01794472bf789300df5d9de4f8e8
5angjun <best_collin@naver.com>
2022-03-25 14:15:53 +0900

    폴더 정리

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+
+const home=require('./routes/home');
+
+const PORT=3000;
+
+app.set("views","./views");
+app.set("view engine","ejs");
+
+
+app.use("/",home);
+
+module.exports=app;
