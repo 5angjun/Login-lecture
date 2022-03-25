27a38e3e00adf96b4155fa62ab3f1b06f5144e85
5angjun <best_collin@naver.com>
2022-03-25 14:46:13 +0900

    DOm으로 html

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,14 @@
-console.log("hello42342");
+const id=document.querySelector("#id");
+const psword=document.querySelector("#psword");
+const loginBtn=document.querySelector("button");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req={
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+};
+console.log(id);
